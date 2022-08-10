4b0100346b64e7d26dcf61ed71628e9766cc7ce3
AGP <info@asr-gooyesh.com>
2022-08-10 20:35:59 +0430

    edit : `train.sh` edited.

--- train.sh
+++ train.sh
@@ -1,15 +1,17 @@
-export model_folder="/home/tensorflow/parsT5/t5-fa-model"
-export cache_folder="/home/tensorflow/.cache/t5-fa-model"
-export train_file="/home/tensorflow/text-data/informal.txt"
+MODEL_DIR="/home/tensorflow/parsT5/t5-fa"
+CACHE_DIR="/home/tensorflow/.cache/t5-fa/puramini"
+TRAINING_FILE="/mnt/t5-fa/data/all_train.txt"
+VALIDATION_FILE="/mnt/t5-fa/data/all_test.txt"
 
 python run_t5_mlm_flax.py \
-    --model_name_or_path=$model_folder \
-    --output_dir=$model_folder \
-    --cache_dir=$cache_folder \
+    --model_name_or_path=$MODEL_DIR \
+    --output_dir=$MODEL_DIR \
+    --cache_dir=$CACHE_DIR \
     --model_type="t5" \
-    --config_name=$model_folder \
-    --tokenizer_name=$model_folder \
-    --train_file=$train_file \
+    --config_name=$MODEL_DIR \
+    --tokenizer_name=$MODEL_DIR \
+    --train_file=$TRAINING_FILE \
+    --validation_file=$VALIDATION_FILE \
     --max_seq_length="256" \
     --per_device_train_batch_size="64" \
     --per_device_eval_batch_size="64" \
@@ -22,7 +24,7 @@ python run_t5_mlm_flax.py \
     --warmup_steps="2000" \
     --logging_steps="500" \
     --save_steps="2500" \
-    --resume_from_checkpoint=$model_folder \
+    --resume_from_checkpoint=$MODEL_DIR \
     --preprocessing_num_workers=20 \
     # --dataset_name="oscar" \
     # --dataset_config_name="unshuffled_deduplicated_fa" \
